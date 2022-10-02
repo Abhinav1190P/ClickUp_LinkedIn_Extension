986f06d1829120db8d2ec3a7ab9d4397868154d2
unknown <pandeysandeep1190@gmail.com>
2022-10-02 20:05:14 +0530

    save

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -30,20 +30,47 @@ function allElementsFromPoint(x, y) {
 document.addEventListener('contextmenu', e => {
 
     var d = document.querySelectorAll('.relative')
-    for (var i = 0; i < d.length; i++) {
-        var g = JSON.stringify(d[i].innerHTML.toString())
-        if (g.includes('data-urn=')) {
-            var fi = document.createElement('div')
-            fi.className = 'abhi' + Math.floor(Math.random() * 1000)
-            fi.innerText = "Hello this is me"
-            fi.style.cursor = 'pointer'
-            fi.onclick = myFunc
-            d[i].appendChild(fi)
+
+
+
+
+    if (d.length > 0) {
+        for (var i = 0; i < d.length; i++) {
+            var g = JSON.stringify(d[i].innerHTML.toString())
+            if (g.includes('data-urn=')) {
+                var fi = document.createElement('div')
+                fi.className = 'abhi' + Math.floor(Math.random() * 1000)
+                fi.innerText = "Hello this is me"
+                fi.style.cursor = 'pointer'
+                fi.onclick = myFunc
+                d[i].appendChild(fi)
+            }
+        }
+    }
+
+    var f = document.querySelectorAll('.occludable-update')
+
+
+    if (f.length > 0) {
+      
+        for (var i = 0; i < f.length; i++) {
+            var g = JSON.stringify(f[i].innerHTML.toString())
+         
+            if (g.includes('data-urn=')) {
+                var fi = document.createElement('div')
+                fi.className = 'abhi' + Math.floor(Math.random() * 1000)
+                fi.innerText = "Hello this is me"
+                fi.style.cursor = 'pointer'
+                fi.onclick = myFunc
+                f[i].appendChild(fi)
+            }
         }
     }
 
 
 })
+
+
 const myFunc = (e) => {
 
     var classN = e.target.getAttribute('class')
